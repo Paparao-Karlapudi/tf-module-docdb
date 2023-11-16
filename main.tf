6bfe07222a6c03fb6161a4f19d3261aa0f606ecd
pappik <pappik@gmail.com>
2023-11-15 19:56:28 -0500

    documentdb

--- main.tf
+++ main.tf
@@ -54,3 +54,15 @@ resource "aws_docdb_cluster_instance" "cluster_instances" {
   tags = merge(local.common_tags, { Name = "${var.env}-docdb-cluster-instance-${count.index}" } )
 
 }
+
+resource "aws_ssm_parameter" "docdb_url_catalogue" {
+  name  = "${var.env}.catalogue.DOCDB_URL"
+  type  = "String"
+  value = "mongodb://${data.aws_ssm_parameter.DB_ADMIN_USER.value}:${data.aws_ssm_parameter.DB_ADMIN_PASS.value}@${aws_docdb_cluster.docdb.endpoint}:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+}
+
+resource "aws_ssm_parameter" "docdb_url_user" {
+  name  = "${var.env}.user.DOCDB_URL"
+  type  = "String"
+  value = "mongodb://${data.aws_ssm_parameter.DB_ADMIN_USER.value}:${data.aws_ssm_parameter.DB_ADMIN_PASS.value}@${aws_docdb_cluster.docdb.endpoint}:27017/user?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+}
